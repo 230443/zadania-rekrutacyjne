df03a623707e48f256f6afda6e77fa24e037415c
Daniel <daniel@example.pl>
2021-10-24 20:50:19 +0200

    Add admin panel
    4.	Dodaj Admin panel dla stworzonych modeli

--- mysite/cats/admin.py
+++ mysite/cats/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
+from .models import Cat, Hunting, Prey, PreyType
+
+
 # Register your models here.
+
+
+class PreyInline(admin.StackedInline):
+    model = Prey
+    extra = 1
+
+
+class HuntingAdmin(admin.ModelAdmin):
+    model = Hunting
+    inlines = [PreyInline]
+
+
+admin.site.register(Hunting, HuntingAdmin)
+admin.site.register(Cat)
+admin.site.register(PreyType)
