8744b67ccc07a8ef64d393d758c3c0e3cea510f7
Daniel <daniel@example.pl>
2021-10-24 20:28:38 +0200

    Add models and migrations
    3.	Dodaj modele i stwórz migracje na podstawie załączonego opisu

---
+++ mysite/cats/models.py
@@ -0,0 +1,36 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Cat(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=50)
+    color = models.CharField(max_length=20)
+    is_male = models.BooleanField()
+
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+        if Cat.objects.filter(user=self.user).count() < 4:
+            super(Cat, self).save()
+        else:
+            raise Exception(f'{self.user} has already 4 cats. No more are allowed.')
+
+    def __str__(self):
+        return self.name
+
+
+class Hunting(models.Model):
+    cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
+    start_date = models.DateTimeField()
+    duration = models.DurationField()
+
+
+class PreyType(models.Model):
+    type_name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.type_name
+
+
+class Prey(models.Model):
+    type = models.ForeignKey(PreyType, on_delete=models.PROTECT)
+    hunting = models.ForeignKey(Hunting, on_delete=models.CASCADE)
