e29cad94940059ea494b48a70951bb9e2fd1ad1f
Daniel <daniel@example.pl>
2021-10-24 23:28:50 +0200

    Add user view.
    7.	Za pomocą DRF(Django Rest framework) stwórz widok do wyświetlania danych użytkownika oraz wszystkich kotów dla danego użytkownika

--- mysite/cats/serializers.py
+++ mysite/cats/serializers.py
@@ -1,11 +1,21 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 
+from .models import Cat
+
+
+class CatSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cat
+        fields = ['id', 'name', 'color', 'is_male']
+
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
+    cats = CatSerializer(source='cat_set', many=True, read_only=True)
+
     class Meta:
         model = User
-        fields = ['url', 'username', 'email', 'groups']
+        fields = ['url', 'username', 'email', 'groups', 'cats']
 
 
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
